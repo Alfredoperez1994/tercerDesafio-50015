6c049918a514405f5aef787ca8b4303ffd47521d
Alfredo Perez <pereztato@hotmail.com.ar>
2023-12-13 23:53:53 -0300

    segundo desafio

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
+const fs = require("fs").promises;
+
 class ProductManager {
 
     static ultId = 0;
- 
-    constructor() {
+
+    constructor(path) {
         this.products = [];
+        this.path = path;
     }
 
 
-    addProduct(title, description, price, img, code, stock) {
+    async addProduct(nuevoObjeto) {
+        let { title, description, price, img, code, stock } = nuevoObjeto;
 
-        
-       
-        if(!title || !description || !price || !img || !code || !stock){
-            console.log("Todos los campos son obligatorios"); 
+        if (!title || !description || !price || !img || !code || !stock) {
+            console.log("Todos los campos son obligatorios");
             return;
         }
 
-   
-        if(this.products.some(item => item.code === code)){
+
+        if (this.products.some(item => item.code === code)) {
             console.log("El codigo debe ser unico");
             return;
         }
 
-     
+
 
         const newProduct = {
             id: ++ProductManager.ultId,
@@ -38,6 +40,8 @@ class ProductManager {
 
         this.products.push(newProduct);
 
+        await this.guardarArchivo(this.products);
+
     }
 
     getProducts() {
@@ -47,47 +51,61 @@ class ProductManager {
     getProductById(id) {
         const product = this.products.find(item => item.id === id);
 
-        if(!product) {
+        if (!product) {
             console.log("Producto no encontrado");
         } else {
             console.log("Siiiiii, lo encontramos: ", product);
         }
-        
+
+    }
+
+    async leerArchivo() {
+        try {
+            const respuesta = await fs.readFile(this.path, "utf-8");
+            const arrayProductos = JSON.parse(respuesta);
+            return arrayProductos;
+        } catch (error) {
+            console.log("Error al guardar el archivo", error);
+        }
+
+    }
+
+    async guardarArchivo(arrayProductos) {
+        try {
+            await fs.writeFile(this.path, JSON.stringify(arrayProductos, null, 2));
+        } catch (error) {
+            console.log("Error al guardar el archivo", error);
+        }
     }
 
 
 }
 
-
-
-
-const manager = new ProductManager();
-
-
+const manager = new ProductManager("./productos.json");
 
 manager.getProducts();
 
 
+const alimentoPerro = {
+    title: "dogchow",
+    description: "lo mejor para tu perro",
+    price: 200,
+    img: "sin imagen",
+    code: "abc123",
+    stock: 50
+}
 
-manager.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc123", 25);
+manager.addProduct(alimentoPerro);
 
+const alimentoGato = {
+    title: "catchow",
+    description: "lo mejor para tu gato",
+    price: 400,
+    img: "sin imagen",
+    code: "abc124",
+    stock: 30
+}
 
-
-
-manager.addProduct("dogui", "lo mas rico para tu perro", 200, "sin imagen", "abc124", 50);
-
-
-manager.addProduct("gati", "lo mas rico para tu gato", 200, "sin imagen", "abc125", 50);
-
-
+manager.addProduct(alimentoGato);
 
 manager.getProducts();
-
-
-
-manager.addProduct("gati", "lo mas rico para tu gato", 200, "sin imagen", "abc125", 50);
-
-
-
-manager.getProductById(2);
-manager.getProductById(50);
